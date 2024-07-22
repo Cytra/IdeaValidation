76004ab17fe95369f0c52fba7ab7168a6b989cd5
Martynas Samuilovas <samuilovas@gmail.com>
2024-07-22 09:37:46 +0300

    test

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -10,7 +10,7 @@ const ContactForm = () => {
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <p style={{ textAlign: 'center' }}>Thanks for joining!</p>
       </div>
-    );
+    ); 
   }
 
   return (
